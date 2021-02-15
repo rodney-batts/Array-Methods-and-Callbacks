cd27fcae5ae85b1ef9824ccedf297d5d43346d39
rodbatts2 <rodbatts2@msn.com>
2021-02-15 06:09:27 -0500

    Task 3 added but bug not conquered.

--- index.js
+++ index.js
@@ -53,8 +53,14 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(/* code here */ getFinals, fifaData, ) {
+    /* code here */ 
+    const years = [];
+    fifaData.forEach(years){
+        getFinals();
+        years.push(fifaData[0]);
+    };
+    };
 }
 
 
